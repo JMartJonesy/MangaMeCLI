8847dfd2e3fa21ce9df217acb87753b98cdc70ee
jem4687 <jem4687@rit.edu>
2015-12-22 02:44:57 -0700

    Downloads seem to be working properly

--- mangaMe.cpp
+++ mangaMe.cpp
@@ -1,11 +1,12 @@
 #define BOOST_NETWORK_ENABLE_HTTPS
 // STD INCLUDES
+#include <map>
 #include <vector>
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <iostream>
-#include <map>
+#include <algorithm>
 
 // MY INCLUDES
 #include "MS-Wrapper.hpp"
@@ -39,6 +40,7 @@ auto mashapeHeader = boost::network::header("X-Mashape-Key", "av7yog4uPAmshcKLni
 fs::path baseDownloadPath(fs::current_path());
 string siteId = "mangareader.net"; // change later
 stringmap mangaList;
+stringmap printList;
 
 // FUNCTIONS
 void printDownloadStatus(int curr, int outOf)
@@ -68,6 +70,7 @@ string getResponseBody(string uri)
 	return body(client.get(request));
 }
 
+// Returns responsebody as property tree
 propTree::ptree getJsonTree(string uri)
 {
 	stringstream ss;
@@ -77,54 +80,60 @@ propTree::ptree getJsonTree(string uri)
 	return pt;
 }
 
+// TODO: Maybe short circuit downloadChapters if chapter isnt found
+// Downloads a single chapter
 void downloadChapter(string manga, int chapter)
 {
-	fs::path mangaPath = baseDownloadPath;
-	mangaPath /= manga;
 	propTree::ptree pt = getJsonTree(endPoints::getChapter(siteId, manga, to_string(chapter)));
-	mangaPath /= to_string(chapter) + "-" + pt.get<string>("name");
-	cout << "Downloading chapter " << chapter << "-" << pt.get<string>("name") << endl;
-	if(!exists(mangaPath) || !is_directory(mangaPath))
+	if(pt.count("name") != 0)
 	{
-		int pageCount = 0;
-		fs::create_directory(mangaPath);
-		BOOST_FOREACH(propTree::ptree::value_type &v, pt.get_child("pages"))
-			for(auto it = v.second.begin(); it != v.second.end(); ++it)		
-				if(it->first.compare("url") == 0)
-					++pageCount;
-		BOOST_FOREACH(propTree::ptree::value_type &v, pt.get_child("pages"))
+		fs::path mangaPath = baseDownloadPath;
+		mangaPath /= manga;
+		mangaPath /= to_string(chapter) + "-" + pt.get<string>("name");
+		cout << "Downloading chapter " << chapter << "-" << pt.get<string>("name") << endl;
+		if(!exists(mangaPath) || !is_directory(mangaPath))
 		{
-			string page = "";
-			for(auto it = v.second.begin(); it != v.second.end(); ++it)		
+			int pageCount = 0;
+			fs::create_directory(mangaPath);
+			BOOST_FOREACH(propTree::ptree::value_type &v, pt.get_child("pages"))
+				for(auto it = v.second.begin(); it != v.second.end(); ++it)		
+					if(it->first.compare("url") == 0)
+						++pageCount;
+			BOOST_FOREACH(propTree::ptree::value_type &v, pt.get_child("pages"))
 			{
-				if(it->first.compare("url") == 0)
+				string page = "";
+				for(auto it = v.second.begin(); it != v.second.end(); ++it)		
 				{
-					string filename = mangaPath.string() + "/" + page + ".jpg";
-					ofstream ofs(filename.c_str());
-					ofs << static_cast<string>(getResponseBody(it->second.get_value<string>())) << endl;
-					ofs.close();
-					printDownloadStatus(stoi(page), pageCount);
-				}
-				else
-				{
-					page = it->second.get_value<string>();
+					if(it->first.compare("url") == 0)
+					{
+						string filename = mangaPath.string() + "/" + page + ".jpg";
+						ofstream ofs(filename.c_str());
+						ofs << static_cast<string>(getResponseBody(it->second.get_value<string>())) << endl;
+						ofs.close();
+						printDownloadStatus(stoi(page), pageCount);
+					}
+					else
+					{
+						page = it->second.get_value<string>();
+					}
 				}
 			}
 		}
-	}
-	else
-	{
-		cout << "Chapter " << chapter << "already downloaded" << endl;
+		else
+		{
+			cout << "Chapter already downloaded" << endl;
+		}
 	}
 }
 
+// Downloads chapters in range from sChaptert to eChapter
 void downloadChapters(string manga, int sChapter, int eChapter)
 {
 	bool good = true;
 	try
 	{
 		fs::path mangaPath = baseDownloadPath;
-		mangaPath /= manga;
+		mangaPath /= printList[manga];
 		if(!exists(mangaPath) || !is_directory(mangaPath))
 		{
 			cout << "Creating folder: " << mangaPath << endl;
@@ -148,6 +157,7 @@ void downloadChapters(string manga, int sChapter, int eChapter)
 			downloadChapter(manga, i);
 }
 
+// Downloads all chapters of given manga
 void downloadManga(string manga)
 {
 	int chapterCount = 0;
@@ -164,6 +174,7 @@ void downloadManga(string manga)
 	downloadChapters(manga, 1, chapterCount);
 }
 
+// Prints out search results for query
 void searchManga(string query)
 {
 	bool results = false;
@@ -186,18 +197,20 @@ void searchManga(string query)
 	}
 }
 
+// Prints all manga found on MangaReader.net
 void listManga()
 {
-	for(auto it = mangaList.begin(); it != mangaList.end(); ++it)
+	for(auto it = printList.begin(); it != printList.end(); ++it)
 		cout << it->first << endl;
 }
 
+// Creates local list of manga from MangaReader.net
 void setupMangaList()
 {
 	propTree::ptree pt = getJsonTree(endPoints::getMangaList(siteId));
 	BOOST_FOREACH(propTree::ptree::value_type &v, pt.get_child(""))
 	{
-		string key, value;
+		string key, value, upperK;
 		for(auto it = v.second.begin(); it != v.second.end(); ++it)
 		{
 			if(it->first.compare("mangaId") == 0)
@@ -207,6 +220,9 @@ void setupMangaList()
 			else
 			{
 				key = it->second.get_value<string>();
+				upperK = key;
+				transform(key.begin(), key.end(), key.begin(), ::tolower);
+				printList.emplace(key, upperK);
 				mangaList.emplace(key, value);
 			}
 		}
@@ -214,8 +230,7 @@ void setupMangaList()
 }
 
 int main(int argc, char *argv[]) {
-	// Checks for download folder and creates it if
-	// doesn't exist
+	// Checks for download folder and creates it if doesn't exist
 	try
 	{
 		baseDownloadPath /= "manga";
@@ -244,6 +259,7 @@ int main(int argc, char *argv[]) {
 
 	try 
 	{
+		// Setup and Main Menu
 		setupMangaList();
 		int input = -1;
 		string choice = "";
@@ -265,12 +281,44 @@ int main(int argc, char *argv[]) {
 					cin >> choice;
 					searchManga(choice);
 					break;	
+				case 3:
+					int sInput = 0;
+					cout << "Input manga: ";
+					cin >> choice;
+					transform(choice.begin(), choice.end(), choice.begin(), ::tolower);
+					if(mangaList.find(choice) == mangaList.end())
+					{
+						cout << choice << " not found" << endl;
+						break;
+					}
+					int sChapter = 0;
+					int eChapter = 0;
+					cout << "Download" << endl;
+					cout << "1. Entire Manga" << endl;
+					cout << "2. Single Chapter" << endl;
+					cout << "3. Multiple Chapters" << endl;
+					cout << "0. Cancel" << endl;
+					cout << "Input: ";
+					cin >> sInput;
+					switch(sInput)
+					{
+						case 1:
+							downloadManga(choice);
+							break;
+						case 2:
+							cout << "Input chapter: ";
+							cin >> sChapter;
+							downloadChapters(choice, sChapter, sChapter);
+							break;
+						case 3:
+							cout << "Input start chapter: ";
+							cin >> sChapter;
+							cout << "Input end chapter: ";
+							cin >> eChapter;
+							downloadChapters(choice, sChapter, eChapter);
+					}
+					
 			}
-			/*
-			string choice = searchManga("naruto");
-			if(choice.compare("None") != 0)
-				cout << choice << endl;
-			*/
 		}
 	}
 	catch (std::exception &e) {
